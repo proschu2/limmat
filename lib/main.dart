32191f6d0b9ad93d883a20474acbca909f70821c
proschu2 <github@sanziomonti.xyz>
2024-08-13 23:52:23 +0200

    add app_check with reecaptcha etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:limmat/widgets/main_view.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  if (kIsWeb) {
+    final String recaptchaSiteKey = dotenv.env['RECAPTCHA_SITE_KEY'] ?? '';
+    print('Using reCAPTCHA v3 site key: $recaptchaSiteKey');
+
+    if (recaptchaSiteKey.isNotEmpty) {
+      await FirebaseAppCheck.instance.activate(
+        webProvider: ReCaptchaV3Provider(recaptchaSiteKey),
+      );
+    }
+  } else {
+    print('No Web');
+    await FirebaseAppCheck.instance
+        .activate(androidProvider: AndroidProvider.playIntegrity);
+  }
+
   runApp(const MyApp());
 }
 
