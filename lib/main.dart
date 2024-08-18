3d7b5b909b70de7795aaf9ccc03fdfa87846b964
proschu2 <github@sanziomonti.xyz>
2024-08-18 17:52:44 +0200

    update workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:limmat/widgets/main_view.dart';
